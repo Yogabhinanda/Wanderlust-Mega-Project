5701ed2cda2363970c8c052595a94592d94234ff
Yogabhinanda <65008198+Yogabhinanda@users.noreply.github.com>
2025-05-02 13:27:12 +0530

    Update install_tools.sh

--- terraform/install_tools.sh
+++ terraform/install_tools.sh
@@ -4,7 +4,7 @@
 sudo apt update
 sudo apt install -y fontconfig openjdk-17-jre 
 
-# Jenkins installation
+# Jenkins master installation
 sudo wget -O /usr/share/keyrings/jenkins-keyring.asc \
   https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key
 echo "deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc]" \
@@ -39,6 +39,21 @@ sudo snap install aws-cli --classic
 
 # Helm installation
 sudo snap install helm --classic
+
+# EKS ctl install
+curl --silent --location "https://github.com/weaveworks/eksctl/releases/latest/download/eksctl_$(uname -s)_amd64.tar.gz" | tar xz -C /tmp
+sudo mv /tmp/eksctl /usr/local/bin
+eksctl version
+
+# Install and configure SonarQube (Master machine)
+docker run -itd --name SonarQube-Server -p 9000:9000 sonarqube:lts-community
+
+# Install Trivy 
+sudo apt-get install wget apt-transport-https gnupg lsb-release -y
+wget -qO - https://aquasecurity.github.io/trivy-repo/deb/public.key | sudo apt-key add -
+echo deb https://aquasecurity.github.io/trivy-repo/deb $(lsb_release -sc) main | sudo tee -a /etc/apt/sources.list.d/trivy.list
+sudo apt-get update -y
+sudo apt-get install trivy -y
 
 # Kubectl installation
 sudo snap install kubectl --classic
