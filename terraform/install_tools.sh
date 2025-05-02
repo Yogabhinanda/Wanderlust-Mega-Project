21aaa6b833dddd9fcda89791bb29721580cd2ac4
Yogabhinanda <65008198+Yogabhinanda@users.noreply.github.com>
2025-05-02 17:18:17 +0530

    Update install_tools.sh

--- terraform/install_tools.sh
+++ terraform/install_tools.sh
@@ -48,12 +48,5 @@ eksctl version
 # Install and configure SonarQube (Master machine)
 docker run -itd --name SonarQube-Server -p 9000:9000 sonarqube:lts-community
 
-# Install Trivy 
-sudo apt-get install wget apt-transport-https gnupg lsb-release -y
-wget -qO - https://aquasecurity.github.io/trivy-repo/deb/public.key | sudo apt-key add -
-echo deb https://aquasecurity.github.io/trivy-repo/deb $(lsb_release -sc) main | sudo tee -a /etc/apt/sources.list.d/trivy.list
-sudo apt-get update -y
-sudo apt-get install trivy -y
-
 # Kubectl installation
 sudo snap install kubectl --classic
