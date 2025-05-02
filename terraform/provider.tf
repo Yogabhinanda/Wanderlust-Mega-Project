53adbb6ef71910d8ce48fa5e2eaad70df4059e48
Yogabhinanda <65008198+Yogabhinanda@users.noreply.github.com>
2025-05-02 14:03:53 +0530

    Update provider.tf

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -20,7 +20,7 @@ locals {
   region          = "us-west-1"
   name            = "tws-eks-cluster"
   vpc_cidr        = "10.0.0.0/16"
-  azs             = ["us-west-1a", "us-west-1b"]
+  azs             = ["us-west-1c", "us-west-1b"]
   public_subnets  = ["10.0.1.0/24", "10.0.2.0/24"]
   private_subnets = ["10.0.3.0/24", "10.0.4.0/24"]
   intra_subnets   = ["10.0.5.0/24", "10.0.6.0/24"]
